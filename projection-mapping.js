920f5b6744b9fc111d8a1b70b458f9e662672c9b
DrewG84 <drew.genel@gmail.com>
2024-11-18 03:24:41 -0500

    decor piece object
    made an object to draw decor objects when given coords and a type

--- projection-mapping.js
+++ projection-mapping.js
@@ -17,6 +17,18 @@ let detectorArray = [
 // array of shape objects
 let shapeArray = [];
 
+// media
+let parts = {
+  windows: [],
+  doors: [],
+  chimneys: [],
+  roofs: [],
+  scaffolds: [],
+  tallPlants: [],
+  shortPlants: [],
+};
+let windowArray = ["/media/doorA.gif"];
+
 // units
 let paperWidth = 1080;
 let inch = 100;
@@ -27,11 +39,22 @@ let quadMap;
 let whiteMap;
 
 
+function preload() {
+    
+  for (let i = 0; i < windowArray.length; i++) {
+    parts.windows[i] = loadImage(windowArray[i]);
+  }
+  
+  
+}
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   // graphics buffers
   shapeBuffer = createGraphics(11 * inch, 8.5 * inch);
   decorBuffer = createGraphics(11 * inch, 8.5 * inch);
+  projectionBuffer = createGraphics(11 * inch, 8.5 * inch);
   saveBuffer = createGraphics(8.5 * inch, 8.5 * inch); // this one can go to the neighborhood screen
   
   
@@ -61,18 +84,20 @@ function setup() {
 function draw() {
   background(0);
 
+  decorBuffer.clear();
+
   // draw all of the shape arrays to the correct buffers
   for (let i = 0; i < shapeArray.length; i++) {
     shapeArray[i].display();
   }
   
-  //image(shapeBuffer, 0, 0);
-  //image(decorBuffer, 0, 0);
+  projectionBuffer.image(shapeBuffer, 0, 0);
+  projectionBuffer.image(decorBuffer, 0, 0);
 
   // draw projection mapping
   whiteMap.display(color('white'));
   let scalar = 0.95;
-  quadMap.displayTexture(shapeBuffer, 0, 0, 11 * inch * scalar, 8.5 * inch * scalar);
+  quadMap.displayTexture(projectionBuffer, 0, 0, 11 * inch * scalar, 8.5 * inch * scalar);
   
 }
 
@@ -109,13 +134,24 @@ function updateData() {
   }
 }
 
+function easeOutElastic(x){
+  const c4 = (2 * Math.PI) / 3;
+  return x === 0
+    ? 0
+    : x === 1
+    ? 1
+    : Math.pow(2, -10 * x) * Math.sin((x * 10 - 0.75) * c4) + 1;
+}
+
 // interprets data and draws things to the proper buffers. this is what will do all the proc gen too
 class Shape {
   constructor(x, y, size, index) {
     this.tl = [x, y]; // top left point
     this.tr = [x + size, y]; // top right point
     this.bl = [x, y + size]; // bottom left point
-    this.br = [x + size, y + size];  // tbottom right point
+    this.br = [x + size, y + size];  // bottom right point
+
+    this.center = [x + 0.5 * size, y + 0.5 * size];
     
     this.col = "white";
     this.shape = "blank";
@@ -124,11 +160,14 @@ class Shape {
     
     this.size = size;
     this.index = index;
+
+    this.decorArray = [];
     
     this.updateShape();
   }
   
   updateShape() {
+    this.decorArray = [];
     // gets data from the data array
     this.col = detectorArray[this.index][0];
     this.shape = detectorArray[this.index][1];
@@ -137,6 +176,7 @@ class Shape {
     switch (this.shape) {
       case "square" :
         this.vertexArray = [this.tl, this.tr, this.br, this.bl];
+        this.decorArray.push(new Decor(this.center[0], this.center[1], "window"));
         break;
       case "triTL" :
         this.vertexArray = [this.tl, this.tr, this.bl];
@@ -171,5 +211,77 @@ class Shape {
     // draw placed animations to the decorBuffer
     decorBuffer.fill(0);
     decorBuffer.text(this.index, this.tr[0], this.tr[1]);
+    for (let i = 0; i < this.decorArray.length; i++) {
+      this.decorArray[i].display();
+    }
+  }
+}
+
+class Decor {
+  constructor(x, y, type) {
+    this.x = x;
+    this.y = y;
+    this.type = type;
+    
+    this.scaling = 1;
+    this.img = null;
+    this.sound = null;
+    
+    this.scaling = 0;
+    this.time = 0;
+    this.timer = 0;
+    this.start = random(10, 100);
+    
+    switch (this.type) {
+      case "door":
+        
+        break;
+      case "window":
+        this.img = random(parts.windows);
+        this.sound = null;
+        break;
+      case "chimney":
+        
+        break;
+      case "triTLRoof":
+        
+        break;
+      case "triTRRoof":
+        
+        break;
+      case "blankScaffold":
+        
+        break;
+      case "triTRScaffold":
+        
+        break;
+      case "triTLScaffold":
+        
+        break;
+      case "shortPlant":
+        
+        break;
+      case "tallPlant":
+        
+        break;
+      default:
+        this.img = null;
+        this.sound = null;
+        break;
+    }
+    console.log(this.img);
+  }
+  display() {
+    
+    if (this.timer > this.start) {
+      this.time += 0.01;
+      this.scaling = easeOutElastic(this.time) * 2.5 * inch;
+    } else {
+      this.scaling = 1;
+      this.timer++;
+    }
+    
+    decorBuffer.imageMode(CENTER);
+    decorBuffer.image(this.img, this.x, this.y, this.scaling, this.scaling);
   }
 }
